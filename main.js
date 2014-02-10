02929c33ff3d415afb63cc17271d357b002c875d
Bernardo Telles <btelles@google.com>
2014-02-09 20:34:43 -0800

    resolve stuff before controller is initialized

--- main.js
+++ main.js
@@ -1,34 +1,23 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-app.config( function($routeProvider) {
+app.config(function($routeProvider) {
   $routeProvider
-    .when('/map/:country/:state/:city', {
-      templateUrl: 'app.html',
-      controller: 'AppCtrl'
-    })
-    .when('/pizza/:crust/:toppings', {
-      template: "Yum!!!",
-      redirectTo: function(routeParams, path, search) {
-        console.log(routeParams);
-        console.log(path);
-        console.log(search);
-        return '/' + routeParams.crust
+    .when('/', {
+      template: "<h1>{{model.message}}</h1>",
+      controller: "AppCtrl",
+      resolve: {
+        app: function($q, $timeout) {
+          var defer = $q.defer();
+          $timeout( function() {
+            console.log("hello");
+            defer.resolve();
+          }, 2000);
+          return defer.promise;
+        }
       }
     })
-    .when('/deep', {
-      template: 'Deep dish'
-    })
-    .otherwise( {
-      template: "I have no idea where you are" 
-    });
-})
+});
 
-app.controller('AppCtrl', function($scope, $routeParams) {
-  $scope.model = {
-    title: 'This is my app!!!',
-    message: "And I live in " +
-      $routeParams.country + ", " +
-      $routeParams.state + ", " +
-      $routeParams.city
-  }
+app.controller('AppCtrl', function($scope) {
+  $scope.model = { message: "I'm a great app"};
 });
