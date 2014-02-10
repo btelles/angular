a120c3f8374c409a0c425af928cab3748768f99e
Bernardo Telles <btelles@google.com>
2014-02-09 17:57:49 -0800

    templateUrl, compile: f()

--- main.js
+++ main.js
@@ -2,26 +2,29 @@ var app = angular.module('myApp', []);
 
 app.controller("AppCtrl", function($scope) {});
 
-app.directive('zippy', function() {
+app.directive('dumbPassword', function() {
+
+  var validElement = angular.element("<div>{{model.input}}</div>");
+
+  this.link = function(scope) {
+    scope.$watch("model.input", function(value) {
+      if (value === 'password') {
+        validElement.toggleClass('alert-box alert')
+      }
+    });
+  }
+
   return {
     restrict: 'E',
-    transclude: true,
-    scope: {
-      title: "@"
+    replace: true,
+    compile: function(tElem) {
+      tElem.append(validElement)
+      return link;
     },
 
-    link: function(scope) {
-      scope.isContentVisible = false;
-
-      scope.toggleContent = function() {
-        scope.isContentVisible = !scope.isContentVisible;
-      }
-    },
-
-    template: '<div class="panel">' +
-                "<h3 ng-click='toggleContent()'>{{title}}</h3>" +
-                "<div ng-show='isContentVisible'>Toggled content</div>" +
-              '</div>'
+    templateUrl: 'zippy.html'
   }
 });
+
+
 
