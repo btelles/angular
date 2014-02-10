c6dd4ea11f1dbc5503e2fb76b538aeb420de0d4a
Bernardo Telles <btelles@google.com>
2014-02-09 19:23:33 -0800

    .otherwise() & /map/:country/:state

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 var app = angular.module('myApp', ['ngRoute']);
 
 app.config( function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'app.html',
-    controller: 'AppCtrl'
-  });
+  $routeProvider
+    .when('/map/:country/:state/:city', {
+      templateUrl: 'app.html',
+      controller: 'AppCtrl'
+    })
+    .when('/pizza', {
+      template: "Yum!!!"
+    })
+    .otherwise( {
+      template: "I have no idea where you are" 
+    });
 })
 
-app.controller('AppCtrl', function($scope) {
+app.controller('AppCtrl', function($scope, $routeParams) {
   $scope.model = {
-    title: 'This is my app!!!'
+    title: 'This is my app!!!',
+    message: "And I live in " +
+      $routeParams.country + ", " +
+      $routeParams.state + ", " +
+      $routeParams.city
   }
 });
