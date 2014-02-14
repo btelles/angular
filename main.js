9460a944e6d55c6e36cef922ecb76741aa0472c7
Bernardo Telles <btelles@google.com>
2014-02-13 23:21:18 -0800

    add error directive

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ app.config(function($routeProvider) {
     })
 });
 
+app.directive('error', function($rootScope) {
+  return {
+    restrict: 'E',
+    template: '<div class="alert-box alert" ng-show="isError">Error</div>',
+    link: function(scope) {
+      $rootScope.$on("$routeChangeError", function(event, current, previous, rejection) {
+        scope.isError = true;
+      })
+    }
+  }
+})
+
 var appCtrl = app.controller('AppCtrl', function($rootScope) {
   $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
     console.log(rejection);
