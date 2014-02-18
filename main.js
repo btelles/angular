53648cd010ee5e155f9cdfa9b9f1cd9b3ffcc139
Bernardo Telles <btelles@google.com>
2014-02-17 18:59:31 -0800

    Controller as foo syntax

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 var app = angular.module('app', []);
 
-app.controller("AppCtrl", function() {});
+app.controller("RoomCtrl", function() {
+  this.openDoor = function() {
+    alert("creak");
+  }
+
+  this.buttonTitle = "I'm a button"
+});
