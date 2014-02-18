a416b80d3d3c1d36c9a7b7693d1b7f5bc88c0ece
Bernardo Telles <btelles@google.com>
2014-02-17 19:08:16 -0800

    Make nested directives, and enforce parent requirement

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 var app = angular.module('app', []);
 
-app.controller("RoomCtrl", function() {
-  this.openDoor = function() {
-    alert("creak");
+app.directive("country", function() {
+  return {
+    restrict: "E",
+    controller: function() {
+      this.makeAnnouncement = function(message) {
+        console.log("country says:" + message);
+      }
+    }
   }
+})
 
-  this.buttonTitle = "I'm a button"
-});
+app.directive("state", function() {
+  return {
+    restrict: "E",
+    require: ["^country"],
+    controller: function() {
+      this.makeLaw = function(law) {
+        console.log("State passes law:" + law);
+      }
+    }
+  }
+})
+
+app.directive("city", function() {
+  return {
+    restrict: "E",
+    require: ["^country", "^state"],
+    link: function(scope, element, attrs, ctrls) {
+      ctrls[0].makeAnnouncement("from within the city");
+      ctrls[1].makeLaw("from within the city");
+    }
+  }
+})
