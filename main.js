abb3755b08dafd8f7e6ff7c28d3583852a73f83a
Bernardo Telles <btelles@google.com>
2014-02-17 19:14:02 -0800

    ng-repeat-{start/end}

--- main.js
+++ main.js
@@ -1,35 +1,5 @@
 var app = angular.module('app', []);
 
-app.directive("country", function() {
-  return {
-    restrict: "E",
-    controller: function() {
-      this.makeAnnouncement = function(message) {
-        console.log("country says:" + message);
-      }
-    }
-  }
-})
-
-app.directive("state", function() {
-  return {
-    restrict: "E",
-    require: ["^country"],
-    controller: function() {
-      this.makeLaw = function(law) {
-        console.log("State passes law:" + law);
-      }
-    }
-  }
-})
-
-app.directive("city", function() {
-  return {
-    restrict: "E",
-    require: ["^country", "^state"],
-    link: function(scope, element, attrs, ctrls) {
-      ctrls[0].makeAnnouncement("from within the city");
-      ctrls[1].makeLaw("from within the city");
-    }
-  }
+app.controller("AppCtrl", function($scope) {
+  this.things = ['one','two','three'];
 })
