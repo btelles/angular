432b474a5e372e49e395dd6fe59ef25b17fee2ab
Bernardo Telles <btelles@google.com>
2014-02-22 20:18:31 -0800

    grunt templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,14 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    watch: {
-      files: ['journal.txt'],
-      tasks: ['pat']
-    }
+    files: "interesting.js",
+    compile: "<%= files %>",
+    moreComplicated: "<% _.forEach(files, function(file){ grunt.log.writeln(file)}) %>",
   });
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('pat', function() {
-    grunt.log.writeln('Keep going, you"re great!');
+    grunt.log.writeln(grunt.config.get("compile"));
+    grunt.log.writeln(grunt.config.get("moreComplicated"));
+    grunt.log.writeln(grunt.template.process("<%= files %>"));
+    grunt.log.writeln(grunt.template.today());
   });
 }
